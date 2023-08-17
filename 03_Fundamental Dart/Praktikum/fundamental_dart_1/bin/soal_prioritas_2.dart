86c68ecd7c2c48be43762ce702beb0a3562f2ade
Farhan Maulana <farhan.m2002.fm@gmail.com>
2023-08-18 00:59:12 +0700

    Adding Tugas Fundamental Dart

---
+++ 03_Fundamental Dart/Praktikum/fundamental_dart_1/bin/soal_prioritas_2.dart
@@ -0,0 +1,15 @@
+  // Penggabungan String
+void main() {
+  String String1 = 'Ini';
+  String String2 = 'Adalah';
+  String String3 = 'Bahasa Pemrograman';
+  String String4 = 'Dart';
+  print(String1 + ' ' + String2 + ' ' + String3 + ' ' + String4);
+
+  // Volume Tabung Silinder
+  double phi = 3.14;
+  int jariJari = 7;
+  int tinggi = 10;
+  double volumeTabungSilinder = phi * jariJari * jariJari * tinggi;
+  print('Volume Tabung Silinder = $volumeTabungSilinder');
+}
---
+++ 03_Fundamental Dart/Praktikum/fundamental_dart_1/test/fundamental_dart_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:fundamental_dart_1/fundamental_dart_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
