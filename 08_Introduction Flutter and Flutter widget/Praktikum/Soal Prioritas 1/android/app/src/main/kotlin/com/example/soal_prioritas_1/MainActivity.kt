d9364a1296ab2276f36232f9019c93b630b68b48
Farhan Maulana <farhan.m2002.fm@gmail.com>
2023-09-03 12:38:54 +0700

    Upload tugas 08

---
+++ 08_Introduction Flutter and Flutter widget/Praktikum/Soal Prioritas 1/android/app/src/main/kotlin/com/example/soal_prioritas_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.soal_prioritas_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 08_Introduction Flutter and Flutter widget/Summary.md
@@ -0,0 +1,7 @@
+# Deep Dive Into Dart Object Oriented Programming (exploration)
+
+1. 
+
+2. 
+
+3. 
