8222f0b4f9ce750feef9a6971d30f043d42d389f
Farhan Maulana <farhan.m2002.fm@gmail.com>
2023-11-16 21:45:32 +0700

    Upload tugas UI Testing

---
+++ 20_Ui Testing/praktikum/lib/material_app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum/material_page.dart';
+
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
